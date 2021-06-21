b73f8b00b9e1bf8fb258b9fb9093217c6c1acf54
Rommel Torquator <rommeltorquator09@gmail.com>
2021-06-21 11:13:40 -0400

    - Made the changes requested by Miss Analyn - Added the email features for both buyer and broker

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,9 +10,13 @@ class HomeController < ApplicationController
   def portfolio; end
 
   def transaction
-    @transactions = current_buyer.purchase_transactions if current_buyer
-    @transactions = PurchaseTransaction.where(broker_id: current_broker.id) if current_broker
-    @transactions = PurchaseTransaction.all if current_admin
+    @transactions = if current_buyer
+      current_buyer.purchase_transactions
+    elsif current_broker
+      PurchaseTransaction.where(broker_id: current_broker.id)
+    elsif current_admin
+      PurchaseTransaction.all
+    end
   end
 
   def show_user
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,31 @@
+class UserMailer < ApplicationMailer
+  def buyer_account_created
+    @email = params[:email]
+
+    mail(
+      from: "Stock App <support@gmail.com>",
+      to: @email,
+      subject: "Your account has been successfully created."
+    )
+  end
+
+  def pending_approval_email
+    @email = params[:email]
+
+    mail(
+      from: "Stock App <support@gmail.com>",
+      to: @email,
+      subject: "Pending Admin approval for #{@email}."
+    )
+  end
+
+  def admin_approved_email
+    @email = params[:email]
+
+    mail(
+      from: "Stock App <support@gmail.com>",
+      to: @email,
+      subject: "Account #{@email} has been approved."
+    )
+  end
+end
