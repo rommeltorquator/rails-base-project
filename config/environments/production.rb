de53d6ea321ac687411a7b87da934eb83ecd65e9
jarencudilla <jarencudilla@gmail.com>
2021-07-03 13:13:44 +0800

    [HOTFIX] updated the configuration in the production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "rails_project_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.smtp_settings = { port: 587 }
   config.action_mailer.default_url_options = { :host => 'stockxchng.herokuapp.com' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
