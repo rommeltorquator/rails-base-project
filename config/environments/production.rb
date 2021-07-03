d975d670c958394403744f230bfb49df61f38fff
jarencudilla <jarencudilla@gmail.com>
2021-07-03 22:08:37 +0800

    [Hotfix] Heroku Deployment update - action mailer SMTP settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => <jarencudilla>,
-    :password             => <cnnicwbgtebobyha>,
+    :user_name            => jarencudilla,
+    :password             => cnnicwbgtebobyha,
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
