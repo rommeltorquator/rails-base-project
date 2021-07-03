ef47dee0807041669bb87ea3b027b435d1646eb1
jarencudilla <jarencudilla@gmail.com>
2021-07-03 11:25:11 +0800

    Heroku Deploy - stockxchng app - presentation day

--- db/migrate/20210618160603_change_broker_stock_id_to_stock_id.rb
+++ db/migrate/20210618160603_change_broker_stock_id_to_stock_id.rb
@@ -1,5 +1,5 @@
 class ChangeBrokerStockIdToStockId < ActiveRecord::Migration[6.0]
   def change
-    rename_column :transactions, :broker_stock_id, :stock_id
+    rename_column :purchase_transactions, :broker_stock_id, :stock_id
   end
 end
